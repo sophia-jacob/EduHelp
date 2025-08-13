58e8f727ec74976706672e206d2621ae97b08513
sophia-jacob <81059516+sophia-jacob@users.noreply.github.com>
2025-08-12 20:42:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function checkCalcQuiz(){
     if (document.querySelector('input[name="q5"]:checked')?.value === "x^(n+1)/(n+1) + C") score++;
     document.getElementById("result").textContent = "You score " + score + " out of 5.";
 }
+
+function checkBioQuiz(){
+    let score=0;
+    if (document.querySelector('input[name="q1"]:checked')?.value === "thymine") score++;
+    if (document.querySelector('input[name="q2"]:checked')?.value === "electrontransportchain") score++;
+    if (document.querySelector('input[name="q3"]:checked')?.value === "lipid") score++;
+    if (document.querySelector('input[name="q4"]:checked')?.value === "proteinsynthesis") score++;
+    if (document.querySelector('input[name="q5"]:checked')?.value === "mitochondria") score++;
+    document.getElementById("result").textContent = "You score " + score + " out of 5.";
+}
